538178cd1861cbff1ffebb1aff23f64f78c77bdc
trev4ev <trevoraquino@gmail.com>
2016-05-09 11:00:30 -0700

    add to constructor

--- src/main/ProjectGon.java
+++ src/main/ProjectGon.java
@@ -1,8 +1,41 @@
 package main;
 
-public class ProjectGon {
-	public static void main(String[] args)
-	{
-		//comment
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class ProjectGon extends Canvas implements Runnable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 160;
+	public static final int HEIGHT = 120;
+	public static final int SCALE = 3;
+	
+	private JFrame frame;
+
+	public ProjectGon() {
+		setMaximumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		
+		frame = new JFrame("Project Gon");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.add(this, BorderLayout.CENTER);
+		frame.pack();
+		
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 }
