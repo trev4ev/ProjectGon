835ffff1be954cb80a23329f91a65f80e170cffe
trev4ev <trevoraquino@gmail.com>
2016-05-09 11:24:31 -0700

    add ticks and render

--- src/main/ProjectGon.java
+++ src/main/ProjectGon.java
@@ -2,7 +2,10 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -13,9 +16,10 @@ public class ProjectGon extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	public static final int WIDTH = 160;
 	public static final int HEIGHT = 120;
-	public static final int SCALE = 3;
+	public static final int SCALE = 4;
 	
 	private JFrame frame;
+	public boolean running = false;
 
 	public ProjectGon() {
 		setMaximumSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
@@ -32,10 +36,80 @@ public class ProjectGon extends Canvas implements Runnable {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
+	
+	public synchronized void start() {
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public synchronized void stop() {
+		running = false;
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		long last = System.nanoTime();
+		double nsPerTick = 1000000000D/60D;
 		
+		int frames = 0;
+		int ticks = 0;
+		
+		long lastTimer = System.currentTimeMillis();
+		double delta = 0;
+		
+		
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now-last)/nsPerTick;
+			last = now;
+			boolean shouldRender = false;
+			
+			while(delta >= 1) {
+				ticks++;
+				tick();
+				delta --;
+				shouldRender = true;
+			}
+			
+			if(shouldRender)
+			{
+				frames ++;
+				render();
+			}
+			
+			
+			if(System.currentTimeMillis() - lastTimer >= 1000)
+			{
+				lastTimer += 1000;
+				System.out.println(frames + ", " + ticks);
+				frames = 0;
+				ticks = 0;
+			}
+		}
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null)
+		{
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.black);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String[] args) {
+		new ProjectGon().start();
 	}
 }
